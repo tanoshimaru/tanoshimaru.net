7a4450f3db648c3d521aa9c1d6747524b9d7c369
tanoshimaru <p42f2fza@s.okayama-u.ac.jp>
2024-06-07 00:14:52 +0900

    edit

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "たのしまる",
+    name: "Tanoshimaru",
     image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "こんにちは",
     bio: "こんばんは",
